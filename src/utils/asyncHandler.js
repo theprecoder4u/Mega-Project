f24dc10601f43698423031ab0ded4b8e5e22535c
Preetam Patel <preetampatel620@gmail.com>
2026-09-25 16:49:47 +0530

    utils

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,8 +1,20 @@
-// const asyncHandler = () => {}
+const asyncHandler = (requestHandler) => {
+    (reg, res, next) => {
+        Promise.resolve(requestHandler(reg, res, next)).catch((err) => next(err))
 
-
-export{asyncHandler}
-
-const asyncHandler = (fn) => () => {
-    try
+    }
 }
+
+
+export {asyncHandler}
+
+// const asyncHandler = (fn) => async (req, res, next) => {
+//     try {
+
+//     } catch (error) {
+//         res.status(err.code || 500).json({ 
+//             success: false,
+//             message: error.message })
+//     }
+    
+// }
